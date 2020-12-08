d942857aea8f31b23993ae99347972d2f917e599
dibasdauliya <dibasdauliya@gmail.com>
2020-12-08 21:21:25 +0545

    es5 :D

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
 
     response.on('end', () => {
       const arrData = JSON.parse(data)
-      res.render('index', { data: arrData, isOpen: isOpen, asOf: asOf })
+      res.render('index', { data: arrData, isOpen, asOf })
     })
   })
 })
